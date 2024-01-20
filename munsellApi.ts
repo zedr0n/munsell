0b06f1328014f17d2f3435c0cd4b791ee4791591
zedr0n <zedr0n@protonmail.com>
2024-01-20 17:41:45 +0000

    add other functions

--- munsellApi.ts
+++ munsellApi.ts
@@ -1,15 +1,47 @@
-import type { App } from "obsidian";
+import type { App, RGB } from "obsidian";
 import * as munsell from 'munsell';
+import { RGBSpace } from "munsell/dist/src/colorspace";
 
 export class MunsellApi {
 	public static GetApi(
 		app: App) {
             return {
                 hexToMhvc: (
-                    hex : string                    
+                    hex : string,
+                    rgbSpace : RGBSpace = munsell.SRGB,
+                    threshold : number = 1e-6,
+                    maxIteration : number = 200,
+                    factor : number = 5
                 ) => {
-                    return munsell.hexToMhvc(hex);
+                    return munsell.hexToMhvc(hex, rgbSpace, threshold, maxIteration, undefined, factor);
                 },
+                mhvcToHex: (
+                    h : number,
+                    v : number,
+                    c : number,
+                    rgbSpace : RGBSpace = munsell.SRGB,
+                ) => {
+                    return munsell.mhvcToHex(h, v, c, rgbSpace)
+                },
+                rgbToMhvc: (
+                    r : number,
+                    g : number,
+                    b : number,
+                    rgbSpace : RGBSpace = munsell.SRGB,
+                    threshold : number = 1e-6,
+                    maxIteration : number = 200,
+                    factor : number = 5
+                ) => {
+                    return munsell.rgbToMhvc(r, g, b, rgbSpace, threshold, maxIteration, undefined, factor)
+                },
+                mhvcToRgb: (
+                    h : number,
+                    v : number,
+                    c : number,
+                    rgbSpace : RGBSpace = munsell.SRGB,
+                ) => {
+                    return munsell.mhvcToRgb(h, v, c, rgbSpace)
+                }
             }
     }
 }
